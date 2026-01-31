cffbd0ea809fe75ff0e4e394ec0b43b1570b0dde
Ali <a_kapan@kbtu.kz>
2026-01-31 12:50:42 +0500

    seventh

---
+++ numbers.py
@@ -0,0 +1,31 @@
+age=17
+name="Ali"
+pi=3.14
+print(type(age),type(name),type(pi))
+
+
+
+#int
+a=5
+b=-10000
+c=15201453
+print(type(a),type(b),type(c))
+
+#float
+a=10.0
+b=-25.4
+c=3.141592
+print(type(a),type(b),type(c))
+
+
+#complex
+a=15+5j
+b=25-1j
+c=-10000+235j
+print(type(a),type(b),type(c))
+
+
+import random
+
+print(random.randrange(1, 10))
+
