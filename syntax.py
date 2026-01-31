2a5bd3a1e1f91ce9fb410c675bf3cb626950bf19
Ali <a_kapan@kbtu.kz>
2026-01-31 10:01:09 +0500

    Third

---
+++ syntax.py
@@ -0,0 +1,20 @@
+#it works
+if 3>2:
+    print(1)
+
+#it does not work because space should be more than 4
+'''if 3>2:
+print(2)'''
+#it works
+if 3>2:
+                          print(3)
+'''error                        
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!")'''
+
+#it is comment
+'''it is also comment
+
+
+'''
