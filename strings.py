5b3e08b04fe1a516c9921ce8e786c8f4fd2fbfc5
Ali <a_kapan@kbtu.kz>
2026-01-31 13:25:49 +0500

    Ninth

---
+++ strings.py
@@ -0,0 +1,50 @@
+print("Ali")
+print('Ali')
+
+a="hello"
+print(a)
+
+a='''Ақ киімді денелі ақ сақалды
+соқыр мылқау танымас тірі жанды
+Үсті басы ақ қырау түсі суық
+басқан жері сықырлап келіп қалды'''
+print(a)
+
+print(a[5],a[17])
+
+for x in a:
+    print(x)
+
+print(len(a))
+
+print("Үсті"in a)
+
+b="Hello World!"
+print(b[2:5])
+print(b[:5])
+print(b[2:])
+print(b[-5:-2])
+print(b.upper())
+print(b.lower())
+a=" Hello World! "
+print(a.strip())
+a = "Hello, World!"
+print(a.replace("Hello", "Сәлем"))
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+
+age = 36
+txt = f"My name is Ali, I am {age}"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
