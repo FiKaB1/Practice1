872b30dd4320e939da94590fb125fc85a19819d7
Ali <a_kapan@kbtu.kz>
2026-01-31 10:14:51 +0500

    Fourth

---
+++ comments.py
@@ -0,0 +1,10 @@
+#it is a comment
+print("Hello world")
+'''it is also comment
+but some line
+!
+!'''
+#it is a comment
+print("Hello world") # Hello world
+#print("Hello, World!")
+print("Cheers, Mate!")
